64cad43783fc4080244a6419169841194013ab54
fachruddin <rifqifebrian63@gmail.com>
2023-10-14 15:23:35 +0700

    Function Add Fahma

---
+++ app/models/Fahma_model.php
@@ -0,0 +1,30 @@
+<?php
+    class Fahma_model {
+        private $db;
+
+        public function __construct()
+        {
+            $this->db = new Database;
+        }
+
+
+        public function getAllPenghuni() 
+        {
+            $this->db->query('SELECT * FROM tb_penghuni ');
+            return $this->db->resultSet();
+        }
+   
+
+    public function addFahma($data)
+    {
+        $query="INSERT INTO tb_penghuni VALUES ('', :nama_penghuni, :alamat, :jenis_kelamin, :agama, :foto)";
+        $this->db->query($query);
+        $this->db->bind('nama_penghuni', $data['nama_penghuni']);
+        $this->db->bind('alamat', $data['alamat']);
+        $this->db->bind('jenis_kelamin', $data['rdJK']);
+        $this->db->bind('agama', $data['agama']);
+        $this->db->bind('foto', $data['foto']);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+}
