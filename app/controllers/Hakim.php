6569691987761fab6c0895d012f2782d790cec8b
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-10-14 15:21:47 +0700

    funtion add hakim

--- app/controllers/Hakim.php
+++ app/controllers/Hakim.php
@@ -11,4 +11,47 @@ class Hakim extends Controller
         $this->view('Hakim/index', $data);
         $this->view('templates/footer');
     }
+
+    private function uploadFoto($file)
+    {
+        $namaFile = $file['name'];
+        $ukuranFile = $file['size'];
+        $error = $file['error'];
+        $tmpName = $file['tmp_name'];
+
+        // Check if there is a file uploaded
+        if ($error === 4) {
+            // No file uploaded
+            return "";
+        }
+
+        // Generate a unique name for the file
+        $namaFileBaru = uniqid() . '-' . $namaFile;
+
+        // Move the uploaded file to the desired folder
+        move_uploaded_file($tmpName, 'path/to/your/upload/folder/' . $namaFileBaru);
+
+        return $namaFileBaru;
+    }
+
+    public function addHakim()
+    {
+        // Set form enctype for file upload
+        $data = $_POST;
+        $foto = $_FILES['foto'];
+
+        // Handle file upload
+        $fotoName = $this->uploadFoto($foto);
+
+        // Add data to the model
+        $data['foto'] = $fotoName;
+        if ($this->model('Penghuni_model')->addPenghuni($data) > 0) {
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+            header('Location: http://localhost/PHP-MVC/public/penghuni');
+        } else {
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+            header('Location: http://localhost/PHP-MVC/public/penghuni');
+            exit;
+        }
+    }
 }
