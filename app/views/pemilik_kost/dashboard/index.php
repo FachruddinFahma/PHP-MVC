168f15247e9c748ea5c5e571378a9822b03c1b2f
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-10-25 14:09:24 +0700

    menambah tampilan 4 indicator dashboard
    Indikator rating, jumlah penghuni, jumlah kamar dan total pendapatan. Namun masih tanpa icon

--- app/views/pemilik_kost/dashboard/index.php
+++ app/views/pemilik_kost/dashboard/index.php
@@ -1,3 +1,81 @@
 <section id="home" class="content">
-    <h1>ini adalah halaman dashboard</h1>
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-3 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="stats-icon purple">
+                                    <i class="iconly-boldShow"></i>
+                                    <!-- icon -->
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h6 class="text-muted font-semibold">Rating Kost</h6>
+                                <h6 class="font-extrabold mb-0">4,5</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-3 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="stats-icon blue">
+                                    <i class="iconly-boldProfile"></i>
+                                    <!-- icon -->
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h6 class="text-muted font-semibold">Jumlah Penghuni</h6>
+                                <h6 class="font-extrabold mb-0">64</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-3 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="stats-icon green">
+                                    <i class="iconly-boldAdd-User"></i>
+                                    <!-- icon -->
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h6 class="text-muted font-semibold">Jumlah Kamar</h6>
+                                <h6 class="font-extrabold mb-0">20</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6">
+                <div class="card">
+                    <div class="card-body px-3 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="stats-icon red">
+                                    <i class="iconly-boldBookmark"></i>
+                                    <!-- icon -->
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h6 class="text-muted font-semibold">Total Pendapatan</h6>
+                                <h6 class="font-extrabold mb-0">Rp. 6.000.000</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
 </section>
