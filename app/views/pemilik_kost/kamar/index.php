f378924bccc2b9087d861c3aa6bc6bfeead49edc
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-10-25 09:20:30 +0700

    add modal input data kamar

--- app/views/pemilik_kost/kamar/index.php
+++ app/views/pemilik_kost/kamar/index.php
@@ -53,15 +53,57 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5>Modal title</h5>
+                        <h5>Input Data Kamar</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+                    <div id="progress-data">
+                        <div id="progress-data-kamar" class="active-data actived">
+                            <div class="angka">
+                                <p>1</p>
+                            </div>
+                            <p class="title-progress">Data Kamar</p>
+                        </div>
+                        <div id="progress-foto" class="active-data">
+                            <div class="angka">
+                                <p>2</p>
+                            </div>
+                            <p class="title-progress">Foto Kamar</p>
+                        </div>
+                        <div id="progress-verifikasi" class="active-data">
+                            <div class="angka">
+                                <p>3</p>
+                            </div>
+                            <p>Verifikasi</p>
+                        </div>
+                    </div>
                     <div class="modal-body">
-                        ...
+                        <div class="input mb-3">
+                            <div class="input-group">
+                                <label for="id_kamar">ID Kamar</label>
+                                <input type="text" class="form-control" placeholder="Username" id="id_kamar" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group">
+                                <label for="id_kamar">Nama Kamar</label>
+                                <input type="text" class="form-control" placeholder="Username" id="nama_kamar" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group">
+                                <label for="id_kamar">Fasilitas Kamar</label>
+                                <input type="text" class="form-control" placeholder="Username" id="fasilitas_kamar" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group">
+                                <label for="id_kamar">Ukuran Kamar</label>
+                                <input type="text" class="form-control" placeholder="Username" id="ukuran_kamar" aria-describedby="basic-addon1">
+                            </div>
+                            <div class="input-group">
+                                <label for="id_kamar">Harga Kamar</label>
+                                <input type="text" class="form-control" placeholder="Username" id="id_kamar" aria-describedby="basic-addon1">
+                            </div>
+
+                        </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary me-auto back" data-bs-dismiss="modal">Kembali</button>
+                        <button type="button" class="btn btn-primary next">Selanjutnya</button>
                     </div>
                 </div>
             </div>
