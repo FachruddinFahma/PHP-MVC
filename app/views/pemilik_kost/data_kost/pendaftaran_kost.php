e7f8e3b144896e591c05979d7924858083c4a130
farhanariyanto03 <ariyantofarhan03@gmail.com>
2023-10-25 17:57:07 +0700

    form data kost dan pendaftaraan kost

---
+++ app/views/pemilik_kost/data_kost/pendaftaran_kost.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="http://localhost/PHP-MVC/public/image/project logo j-kost white.png">
+    <title>Halaman Pnedaftaraan Kost</title>
+</head>
+
+<body>
+    <div class="box">
+        <div class="box-content">
+            <div class="form-group">
+                <div class="mb-3 row">
+                    <div class="col-sm-6">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Nama Kost</label>
+                        <input type="text" class="form-control" id="inputPassword">
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="inputPassword" class="col-sm-3 col-form-label">Nama Pemilik</label>
+                        <input type="text" class="form-control" id="inputPassword">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
