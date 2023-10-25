c5c325206dee9f91063eff9b5bd18c724417d2c4
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-10-25 14:51:30 +0700

    menambahkan landing page file

---
+++ app/controllers/Landing_page.php
@@ -0,0 +1,8 @@
+<?php
+class Landing_page extends Controller
+{
+    public function index()
+    {
+        $this->view('landing_page/index');
+    }
+}
---
+++ app/views/landing_page/index.php
@@ -0,0 +1,9 @@
+<head>
+    <link rel="stylesheet" href="http://localhost/PHP-MVC/public/css/landing_page.css">
+</head>
+
+<body>
+    <h1>halo</h1>
+</body>
+
+</html>
