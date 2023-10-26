d50ba26b221012bb9f4190bd62e1c97d43a22116
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-10-26 09:58:33 +0700

    tambah beberapa section pada landing page

--- app/views/landing_page/index.php
+++ app/views/landing_page/index.php
@@ -1,9 +1,168 @@
 <head>
     <link rel="stylesheet" href="http://localhost/PHP-MVC/public/css/landing_page.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1>halo</h1>
+    <section id="why-jkost">
+        <div class="title-why">
+            <h3>Kenapa harus <span>J-Kost</span></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, amet! consectetur adipisicing elit. Facilis, amet!</p>
+        </div>
+        <div id="kumpulan-icon-why">
+            <div class="icon-why">
+                <div class="bg-icon-why">
+                    <i class="ri-shake-hands-fill ic"></i>
+                </div>
+                <h3>Kemudahan</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, recusandae?</p>
+            </div>
+            <div class="icon-why">
+                <div class="bg-icon-why">
+                    <i class="ri-bar-chart-box-fill ic"></i>
+                </div>
+                <h3>Luas</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, recusandae?</p>
+            </div>
+            <div class="icon-why">
+                <div class="bg-icon-why">
+                    <i class="ri-money-dollar-circle-fill ic"></i>
+                </div>
+                <h3>Murah</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, recusandae?</p>
+            </div>
+            <div class="icon-why">
+                <div class="bg-icon-why">
+                    <i class="ri-chat-smile-3-fill ic"></i>
+                </div>
+                <h3>User Friedly</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, recusandae?</p>
+            </div>
+        </div>
+    </section>
+    <section id="fitur-unggulan">
+        <div id="fiturunggulan-tulisan">
+            <h2>Mencari Kost Lebih Cepat, Praktis, dan Aman</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsum quae neque sed voluptatem dolorem illo, dolor minima veniam quo! sit amet consectetur adipisicing elit. Aliquam ipsum quae neque sed voluptatem dolorem illo, dolor minima veniam quo!</p>
+            <div class="kumpulan-fiturunggulan">
+                <div class="list-fitur">
+                    <i class="ri-checkbox-circle-fill ic-fitur"></i>
+                    <p>Costumer Support</p>
+                </div>
+                <div class="list-fitur">
+                    <i class="ri-checkbox-circle-fill ic-fitur"></i>
+                    <p>Harga Terjangkau</p>
+                </div>
+                <div class="list-fitur">
+                    <i class="ri-checkbox-circle-fill ic-fitur"></i>
+                    <p>Banyak Lokasi</p>
+                </div>
+            </div>
+        </div>
+        <div id="fiturunggulan-gambar">
+            <img src=" http://localhost/PHP-MVC/public/image/kumpulan-card.png" alt="kumpulan card">
+        </div>
+    </section>
+    <section id="order">
+        <div class="title-section-order">
+            <h2>Bagaimana <b>cara order?</b></h2>
+        </div>
+        <div class="content-order">
+            <div class="list-order">
+                <div class="gambar-order">
+                    <i class="ri-shopping-bag-3-fill ic-order"></i>
+                </div>
+                <p class="title-order title-order-after">Klik Kost</p>
+                <p class="penjelasan-order">Pilih kost dan klik pesan kost</p>
+            </div>
+            <div class="list-order">
+                <div class="gambar-order">
+                    <i class="ri-shopping-bag-3-fill ic-order"></i>
+                </div>
+                <p class="title-order title-order-after">Klik Kost</p>
+                <p class="penjelasan-order">Pilih kost dan klik pesan kost</p>
+            </div>
+            <div class="list-order">
+                <div class="gambar-order">
+                    <i class="ri-shopping-bag-3-fill ic-order"></i>
+                </div>
+                <p class="title-order">Klik Kost</p>
+                <p class="penjelasan-order">Pilih kost dan klik pesan kost</p>
+            </div>
+        </div>
+    </section>
+    <section id="testimoni">
+        <div class="title-section-testimoni">
+            <h2>Testimoni aplikasi kami</b></h2>
+        </div>
+        <div class="content-testimoni">
+            <div id="kumpulan-card-testimoni">
+                <div class="card-testimoni">
+                    <div class="profile-testimoni">
+                        <div class="gambar-testimoni">
+                            <img src=" http://localhost/PHP-MVC/public/image/kumpulan-card.png" alt="kumpulan card">
+                        </div>
+                        <div class="nama-testimoni">
+                            <p><b>Nico Flassy</b></p>
+                            <p class="jabatan-testimoni">Mahasiswa IT Polije</p>
+                        </div>
+                    </div>
+                    <div class="content-testimoni">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptates debitis, neque deleniti nobis sit consequatur minima facere! Tenetur veniam rem debitis quas sapiente temporibus aut est repudiandae porro fuga!</p>
+                    </div>
+                    <div class="rating-testimoni">
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                    </div>
+                </div>
+                <div class="card-testimoni">
+                    <div class="profile-testimoni">
+                        <div class="gambar-testimoni">
+                            <img src=" http://localhost/PHP-MVC/public/image/kumpulan-card.png" alt="kumpulan card">
+                        </div>
+                        <div class="nama-testimoni">
+                            <p><b>Nico Flassy</b></p>
+                            <p class="jabatan-testimoni">Mahasiswa IT Polije</p>
+                        </div>
+                    </div>
+                    <div class="content-testimoni">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptates debitis, neque deleniti nobis sit consequatur minima facere! Tenetur veniam rem debitis quas sapiente temporibus aut est repudiandae porro fuga!</p>
+                    </div>
+                    <div class="rating-testimoni">
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                    </div>
+                </div>
+                <div class="card-testimoni">
+                    <div class="profile-testimoni">
+                        <div class="gambar-testimoni">
+                            <img src=" http://localhost/PHP-MVC/public/image/kumpulan-card.png" alt="kumpulan card">
+                        </div>
+                        <div class="nama-testimoni">
+                            <p><b>Nico Flassy</b></p>
+                            <p class="jabatan-testimoni">Mahasiswa IT Polije</p>
+                        </div>
+                    </div>
+                    <div class="content-testimoni">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptates debitis, neque deleniti nobis sit consequatur minima facere! Tenetur veniam rem debitis quas sapiente temporibus aut est repudiandae porro fuga!</p>
+                    </div>
+                    <div class="rating-testimoni">
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                        <i class="ri-star-fill ic-testimoni"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
