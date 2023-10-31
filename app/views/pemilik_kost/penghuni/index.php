369f34de60c0a8b2f06d6d99620058f8cccc1926
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-11-01 06:12:14 +0700

    ubah lebar sidebar & content section

--- app/views/pemilik_kost/penghuni/index.php
+++ app/views/pemilik_kost/penghuni/index.php
@@ -2,9 +2,10 @@
     <div class="row">
         <div class="col-6">
             <?php
+            ini_set('display_errors', 1);
             $username = isset($_SESSION['username']) ? $_SESSION['username'] : '';
             echo "<h3>Hallo, Selamat Datang $username </h3><br>";
-            ?>
+            ?>  
             <h5>Daftar Penghuni</h5>
             <div class="row">
                 <div class="col-lg-6">
@@ -104,57 +105,56 @@
             </div>
         </div>
     </div> -->
-    
+
 
 <section id="home" class="content">
-    
-<div class="col-md-8e p-5" >
-    
 
-<div class="card-body bg-white p-4" style="border-radius: 18px;">
-    
-    <table class="table table-hover small" id="dataTable">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Nama</th>
-          <th scope="col">Kamar</th>
-          <th scope="col">Jenis Kelamin</th>
-          <th scope="col">No HP</th>
-          <th scope="col">Alamat</th>
-          <th scope="col">Aksi</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-        <?php
-        $i = 1;
-    foreach ($data['penghuni']as $d) :
-    ?>
-          <th scope="row"><?= $i ?> </th>
-          <td><?= $d['nama_lengkap'] ?></td>
-          <td><?= $d['id_kamar'] ?></td>
-          <td><?= $d['jenis_kelamin'] ?></td>
-          <td><?= $d['no_hp'] ?></td>
-          <td><?= $d['alamat'] ?></td>
-          <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop<?= $d['id_user'] ?>"><i class="fa-solid fa-circle-info"></i></button>
-          <form method="POST" action="http://localhost/PHP-MVC/public/penghuni/deletePenghuni/<?= $d['id_user']; ?>" style="display:inline;">
-            <button class="btn btn-danger" type="submit" onclick="return confirm('Apakah Anda yakin ingin menghapus penghuni?')"><i class="fa-solid fa-trash"></i></button>
-        </tr>
-        
-        <?php
-        include "modal.php";
-        $i++;
-        endforeach ?>
-      </tbody>
-     
-    </table>
-</div>
+    <div class="col-md-8e p-5">
 
 
+        <div class="card-body bg-white p-4" style="border-radius: 18px;">
+
+            <table class="table table-hover small" id="dataTable">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Kamar</th>
+                        <th scope="col">Jenis Kelamin</th>
+                        <th scope="col">No HP</th>
+                        <th scope="col">Alamat</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php
+                        $i = 1;
+                        foreach ($data['penghuni'] as $d) :
+                        ?>
+                            <th scope="row"><?= $i ?> </th>
+                            <td><?= $d['nama_lengkap'] ?></td>
+                            <td><?= $d['id_kamar'] ?></td>
+                            <td><?= $d['jenis_kelamin'] ?></td>
+                            <td><?= $d['no_hp'] ?></td>
+                            <td><?= $d['alamat'] ?></td>
+                            <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop<?= $d['id_user'] ?>"><i class="fa-solid fa-circle-info"></i></button>
+                                <form method="POST" action="http://localhost/PHP-MVC/public/penghuni/deletePenghuni/<?= $d['id_user']; ?>" style="display:inline;">
+                                    <button class="btn btn-danger" type="submit" onclick="return confirm('Apakah Anda yakin ingin menghapus penghuni?')"><i class="fa-solid fa-trash"></i></button>
+                    </tr>
+
+                <?php
+                            include "modal.php";
+                            $i++;
+                        endforeach ?>
+                </tbody>
+
+            </table>
+        </div>
 
 
 
-</div>
+
+
+    </div>
 </section>
-
