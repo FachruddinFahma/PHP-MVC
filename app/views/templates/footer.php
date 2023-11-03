0c28ba081634fb51d776935414b45ee8f96a1f00
fachruddin <118855404+FachruddinFahma@users.noreply.github.com>
2023-11-03 19:38:53 +0700

    mengukur detil margin

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -12,7 +12,10 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
   new DataTable("#dataTable", {
-    info: false,
+    "columnDefs": [
+        { "visible": false, "targets": [indeks_kolom_tersembunyi] }
+    ]
+    
 
   });
   new DataTable("#example", {
