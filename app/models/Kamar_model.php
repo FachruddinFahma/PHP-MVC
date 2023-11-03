eeb76e54ce2e34b5f92e6db723f8d9d5f8814368
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-11-03 12:25:34 +0700

    tambah data kamar (belom fix)

--- app/models/Kamar_model.php
+++ app/models/Kamar_model.php
@@ -20,20 +20,16 @@ class Kamar_model
         $this->db->bind('id', $id_Penghuni);
         return $this->db->single();
     }
+
     public function addKamar($data)
     {
-        $query = "INSERT INTO tb_user(`id_user`, `nama_lengkap`, `email`, `password`, `tggl_lahir`, 'jenis_kelamin', 'alamat', 'no_hp', 'foto_user', 'id_role') VALUES (':id_user', :nama_lengkap, :email, :pass, :tggl_lahir, :jenis_kelamin, :alamat, :no_hp, :foto_user, :id_role:)";
+        $query = "INSERT INTO tb_kamar(`id_kamar`, `nama_kamar`, `fasilitas_kamar`, `ukuran_kamar`, `harga_kamar`, `status_kamar`) VALUES (:id_kamar, :nama_kamar, :fasilitas_kamar, :ukuran_kamar, :harga_kamar, 'Kosong')";
         $this->db->query($query);
-        $this->db->bind('id_user', $data['id_user']);
-        $this->db->bind('nama_lengkap', $data['nama_lengkap']);
-        $this->db->bind('email', $data['email']);
-        $this->db->bind('pass', $data['pass']);
-        $this->db->bind('tggl_lahir', $data['tggl_lahir']);
-        $this->db->bind('jenis_kelamin', $data['jenis_kelamin']);
-        $this->db->bind('alamat', $data['alamat']);
-        $this->db->bind('no_hp', $data['no_hp']);
-        $this->db->bind('foto_user', $data['foto_user']);
-        $this->db->bind('id_role', $data['id_role']);
+
+        foreach ($data as $key => $value) {
+            $this->db->bind($key, $value);
+        }
+
         $this->db->execute();
         return $this->db->rowCount();
     }
--- app/views/pemilik_kost/kamar/index.php
+++ app/views/pemilik_kost/kamar/index.php
@@ -3,6 +3,7 @@
 </header>
 <section id="kamar" class="content">
     <div id="kamar_content" class="pt-4 px-4">
+        <a href="" class="add" data-bs-toggle="modal" data-bs-target="#modal-identitas">Tambah</a>
         <table id="example" class="display" style="width:100%">
             <thead>
                 <tr>
@@ -49,3 +50,69 @@
         </table>
     </div>
 </section>
+<!-- modal identitas -->
+<div class="modal fade" id="modal-identitas" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Input Data Kamar</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div id="progress-data">
+                <div id="progress-data-kamar" class="active-data actived">
+                    <div class="angka">
+                        <p>1</p>
+                    </div>
+                    <p class="title-progress">Data Kamar</p>
+                </div>
+                <div id="progress-foto" class="active-data">
+                    <div class="angka">
+                        <p>2</p>
+                    </div>
+                    <p class="title-progress">Foto Kamar</p>
+                </div>
+                <div id="progress-verifikasi" class="active-data">
+                    <div class="angka">
+                        <p>3</p>
+                    </div>
+                    <p>Verifikasi</p>
+                </div>
+            </div>
+            <div class="modal-body">
+                <div class="input mb-3">
+                    <form action="">
+                        <div class="input-group">
+                            <label for="id_kamar">ID Kamar</label>
+                            <input type="text" name="id_kamar" class="form-controll" placeholder="Masukan ID Kamar" id="id_kamar">
+                        </div>
+                        <div class="input-group">
+                            <label for="id_kamar">Nama Kamar</label>
+                            <input type="text" name="nama_kamar" class="form-controll" placeholder="Masukan Nama Kamar" id="nama_kamar">
+                        </div>
+                        <div class="input-group">
+                            <label for="id_kamar">Fasilitas Kamar</label>
+                            <input type="text" name="fasilitas_kamar" class="form-controll" placeholder="Masukan Fasilitas Kamar" id="fasilitas_kamar">
+                        </div>
+                        <div class="input-group">
+                            <label for="id_kamar">Ukuran Kamar</label>
+                            <div id="input-ukuran-kamar">
+                                <input type="text" name="ukuran_kamar" class="form-controll" placeholder="Panjang" class="ukuran_kamar">
+                                <!-- <p> X </p>
+                                <input type="text" class="form-controll" placeholder="Lebar" class="ukuran_kamar"> -->
+                            </div>
+                        </div>
+                        <div class="input-group">
+                            <label for="id_kamar">Harga Kamar</label>
+                            <input type="text" name="harga_kamar" class="form-controll" placeholder="Masukan Harga Kamar" id="id_kamar">
+                        </div>
+                        <button type="submit">Tambah</button>
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary me-auto back" data-bs-dismiss="modal">Kembali</button>
+                <button type="button" class="btn btn-primary next" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#modal-foto">Selanjutnya</button>
+            </div>
+        </div>
+    </div>
+</div>
