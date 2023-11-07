6685eecd9daadc39f65332f7ba53eef5858956ed
fachruddin <118855404+FachruddinFahma@users.noreply.github.com>
2023-11-07 20:29:03 +0700

    Update Profile Text

--- app/models/Profile_Model.php
+++ app/models/Profile_Model.php
@@ -14,4 +14,19 @@ class profile_model
         $this->db->bind('id', $id);
         return $this->db->resultSet();
     }
+
+    public function updateUser($id_user, $nama_lengkap, $email, $password, $tggl_lahir, $jenis_kelamin, $alamat, $no_hp)
+{
+    $this->db->query("UPDATE tb_user SET nama_lengkap = :nama_lengkap, email = :email, password = :password, tggl_lahir = :tggl_lahir, jenis_kelamin = :jenis_kelamin, alamat = :alamat, no_hp = :no_hp WHERE id_user = :id_user");
+    $this->db->bind(':id_user', $id_user);
+    $this->db->bind(':nama_lengkap', $nama_lengkap);
+    $this->db->bind(':email', $email);
+    $this->db->bind(':password', $password);
+    $this->db->bind(':tggl_lahir', $tggl_lahir);
+    $this->db->bind(':jenis_kelamin', $jenis_kelamin);
+    $this->db->bind(':alamat', $alamat);
+    $this->db->bind(':no_hp', $no_hp);
+    return $this->db->execute();
+}
+
 }
--- app/views/pemilik_kost/profile/index.php
+++ app/views/pemilik_kost/profile/index.php
@@ -15,61 +15,69 @@
         <div class="row">
             <div class="col-md-3 border-right">
                 <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                    <img class="rounded-circle mt-5" width="150px" src="http://localhost/PHP-MVC/public/image/<?= $penghuni['foto_user'] ?>">
+                    <img class="rounded-circle mt-3 mb-4" style="object-fit: cover;" height="190" width="190px" src="http://localhost/PHP-MVC/public/image/<?= $penghuni['foto_user']?>" id="outputProfile">
                     <span class="font-weight-bold"><?= $penghuni['nama_lengkap'] ?></span>
                     <span class="text-black-50"><?= $penghuni['email'] ?></span>
                 </div>
                 <div class="text-center">
-                    <button class="btn btn-success profile-button" type="button">Ganti Foto</button>
+                    
+                <div class="input-group mb-3">
+  <input type="file" class="form-control" id="imgInpProfile"  aria-label="Upload">
+</div>
+
                 </div>
             </div>
             <?php endforeach; ?>
-
             <div class="col-md-8 border-right ">
                 <div class="p-3 py-5">
+                
                     <div class="d-flex justify-content-between align-items-center mb-3">
                         <h4 class="text-right">Profile Settings</h4>
                     </div>
                     <?php foreach ($data['profile'] as $penghuni) : ?>
+                        <form action="http://localhost/PHP-MVC/public/profile/updateUser/<?= $penghuni['id_user'] ?>" method="post">
                         <div class="row mt-2 mb-4">
                             <div class="col-md-6">
                                 <label class="labels">Nama Lengkap</label>
-                                <input value="<?= $penghuni['nama_lengkap'] ?>" type="text" class="form-control" placeholder="Masukkan Nama Lengkap" value="">
+                                <input value="<?= $penghuni['nama_lengkap'] ?>" name="nama_lengkap" type="text" class="form-control" placeholder="Masukkan Nama Lengkap" value="">
                             </div>
                             <div class="col-md-6">
                                 <label class="labels">Tanggal Lahir</label>
-                                <input type="text" class="form-control" value="<?= $penghuni['tggl_lahir'] ?>" placeholder="Masukkan Tanggal Lahir">
+                                <input type="text" class="form-control" name="tggl_lahir" value="<?= $penghuni['tggl_lahir'] ?>" placeholder="Masukkan Tanggal Lahir">
                             </div>
                         </div>
                         <div class="row mt-2 mb-4">
                             <div class="col-md-6">
                                 <label class="labels">Email</label>
-                                <input type="text" class="form-control" placeholder="Masukkan Email" value="<?= $penghuni['email'] ?>">
+                                <input type="text" class="form-control" name="email" placeholder="Masukkan Email" value="<?= $penghuni['email'] ?>">
                             </div>
                             <div class="col-md-6">
                                 <label class="labels">Password</label>
-                                <input type="password" class="form-control" placeholder="Masukkan Password" value="<?= $penghuni['password'] ?>">
+                                <input type="password" class="form-control" name="password" placeholder="Masukkan Password" value="<?= $penghuni['password'] ?>">
                             </div>
                         </div>
                         <div class="row mt-2 mb-4">    
                             <div class="col-md-6">
                                 <label class="labels">Alamat</label>
-                                <input type="text" class="form-control" placeholder="Masukkan Alamat" value="<?= $penghuni['alamat'] ?>">
+                                <input type="text" class="form-control" name="alamat" placeholder="Masukkan Alamat" value="<?= $penghuni['alamat'] ?>">
                             </div>
                             <div class="col-md-6">
                                 <label class="labels">Nomor HP</label>
-                                <input type="text" class="form-control" placeholder="Masukkan Nomor HP" value="<?= $penghuni['no_hp'] ?>">
+                                <input type="text" class="form-control" name="no_hp" placeholder="Masukkan Nomor HP" value="<?= $penghuni['no_hp'] ?>">
                             </div>
                         </div>
                         <div class="row mt-2">  
                             <div class="col-md-6">
                                 <label class="labels">Kelamin</label>
-                                <input type="text" class="form-control" placeholder="Masukkan Kelamin" value="<?= $penghuni['jenis_kelamin'] ?>">
+                                <input type="text" class="form-control" name="jenis_kelamin" placeholder="Masukkan Kelamin" value="<?= $penghuni['jenis_kelamin'] ?>">
                             </div>
                         </div>  
                     <?php endforeach; ?>
                     <div class="mt-5 text-center">
-                        <button class="btn btn-primary profile-button" type="button">Save Profile</button>
+                    
+                    <input type="hidden" name="id_user" value="<?= $penghuni['id_user'] ?>">
+                    <button type="submit" class="btn btn-primary profile-button">Update Profile</button>
+                    </form>
                     </div>
                 </div>
             </div>
@@ -79,3 +87,12 @@
 
 </body>
 </html>
+
+<script>
+    imgInpProfile.onchange = evt => {
+    const [file] = imgInpProfile.files
+    if (file) {
+      outputProfile.src = URL.createObjectURL(file)
+    }
+  }
+</script>
