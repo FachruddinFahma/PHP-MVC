8cfb84adbcf86beb6acbce8ede2c7cdaa3b638ab
Debby Az-Zahra Firdauzi <116801199+debb4@users.noreply.github.com>
2023-11-07 20:18:14 +0700

    id user update pakek 7 char

--- app/models/Register_model.php
+++ app/models/Register_model.php
@@ -17,14 +17,26 @@
             
         }
         public function addRegisterPemilik($dataPemilik){
-
             $lastid = $this->getlastid();
             $lastidkestring = $lastid[0]["id_user"];
-            $lastid1 = intval($lastidkestring);
-            $finallastid = $lastid1 + 1 ; 
+            $ambilangka3 = substr($lastidkestring,-3);
+            if($ambilangka3 < 9){
+                $lastid2 = intval($ambilangka3);
+                $seklastid = $lastid2+1;
+                $finallastid = 'USER00'.$seklastid;
+            }elseif($ambilangka3 > 99){
+                $lastid2 = intval($ambilangka3);
+                $seklastid = $lastid2 + 1;
+                $finallastid = "USER0".$seklastid;
+            }elseif($ambilangka3 < 1000){
+                $lastid2 = intval($ambilangka3);
+                $seklastid = $lastid2+ 1;
+                $finallastid = "USER".$seklastid;
+            }
+            echo$finallastid;
             
             var_dump($lastid);
-            var_dump($lastid1);
+            var_dump($lastid2);
             var_dump($finallastid);
 
             
@@ -42,21 +54,17 @@
         public function addRegisterPenyewa($dataPenyewa){
             $lastid = $this->getlastid();
             $lastidkestring = $lastid[0]["id_user"];
-            $ambilangka3 = substr($lastidkestring, -4);
+            $ambilangka3 = substr($lastidkestring, -3);
             if($ambilangka3 < 9 ){
                 $lastid2 = intval($ambilangka3);
             $seklastid = $lastid2 + 1 ;
-            $finallastid = 'USER000'.$seklastid;
+            $finallastid = 'USER00'.$seklastid;
             } elseif($ambilangka3 < 99 ) {
                 $lastid2 = intval($ambilangka3);
             $seklastid = $lastid2 + 1 ;
-            $finallastid = 'USER00'.$seklastid;
+            $finallastid = 'USER0'.$seklastid;
                 echo $finallastid;
-        } elseif($ambilangka3 < 999 ) {
-            $lastid2 = intval($ambilangka3);
-        $seklastid = $lastid2 + 1 ;
-        $finallastid = 'USER0'.$seklastid;
-        } elseif($ambilangka3 <10000) {
+        } elseif($ambilangka3 < 1000 ) {
             $lastid2 = intval($ambilangka3);
         $seklastid = $lastid2 + 1 ;
         $finallastid = 'USER'.$seklastid;
