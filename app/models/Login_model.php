422626817501108e82f51304d4001cb8dc3f9c8f
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-11-07 10:40:08 +0700

    Fixing Conflict

--- app/models/Login_model.php
+++ app/models/Login_model.php
@@ -1,31 +1,6 @@
 <?php
-<<<<<<< Updated upstream
-    class Login_model {
-        private $db;
-
-        public function __construct()
-        {
-            $this->db = new Database;
-        }
-
-        public function prosesLogin($email, $password)
-        {
-            $query = "SELECT * FROM tb_user WHERE email = :email AND password = :password";
-            $this->db->query($query);
-            $this->db->bind(':email', $email);
-            $this->db->bind(':password', $password);
-            
-            return $this->db->single();
-        }
-        public function lupaPassword($email, $password){
-            $query = "UPDATE tb_user SET email = :email , password = :password";
-            
-        }
-=======
-class Login_model
-{
+class Login_model{
     private $db;
->>>>>>> Stashed changes
 
     public function __construct()
     {
