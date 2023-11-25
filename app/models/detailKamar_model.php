b890f82223fac9870170c389256f85740bbe42fd
farhanariyanto03 <ariyantofarhan03@gmail.com>
2023-11-25 22:37:27 +0700

    detail kamar

--- app/models/detailKamar_model.php
+++ app/models/detailKamar_model.php
@@ -1,5 +1,5 @@
 <?php
-    class dataKost_model
+    class detailKamar_model
     {
         private $db;
 
@@ -8,10 +8,14 @@
             $this->db = new Database;
         }
 
-        public function getKamarbyIdKost()
+        public function getKamarbyIdKamar($id_kamar)
         {
-            $this->db->query('SELECT * FROM tb_kamar');
-            return $this->db->resultSet();
+            $this->db->query("SELECT tb_kost.alamat, tb_kost.id_user, tb_kamar.fasilitas, tb_kamar.ukuran, tb_kost.peraturan_kost, tb_kost.latitude, tb_kost.longitude
+                FROM tb_kamar
+                JOIN tb_kost ON tb_kamar.id_kost  = tb_kost.id_kost
+                WHERE tb_kamar.id_kamar = :id_kamar");
+            $this->db->bind(':id_kamar', $id_kamar);
+            return $this->db->single();
         }
     }
 ?>
