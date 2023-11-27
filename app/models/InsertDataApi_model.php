0ce7bb30972e919145a82e18a4ba06b30f054ae5
Debby Az-Zahra Firdauzi <78738330+hakimlutfi46@users.noreply.github.com>
2023-11-28 00:14:10 +0700

    add API insert komplain

---
+++ app/models/InsertDataApi_model.php
@@ -0,0 +1,32 @@
+<?php
+class InsertDataApi_model{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+        date_default_timezone_set('Asia/Jakarta'); // Sesuaikan dengan zona waktumu
+    }
+
+    public function addKomplain($id_komplain, $id_user, $jenis_komplain, $tipe_komplain, $deskripsi_komplain, $tggl_komplain){
+        // Jika $tggl_komplain kosong, atur nilainya sebagai waktu saat ini
+        if(empty($tggl_komplain)) {
+            $tggl_komplain = date('Y-m-d H:i:s'); // Mengambil waktu saat ini
+        }
+
+        $this->db->query('INSERT INTO tb_komplain (id_komplain, id_user, jenis_komplain, tipe_komplain, deskripsi_komplain, tggl_komplain) VALUES (:id_komplain, :id_user, :jenis_komplain, :tipe_komplain, :deskripsi_komplain, :tggl_komplain)');
+        $this->db->bind(':id_komplain', $id_komplain);
+        $this->db->bind(':id_user', $id_user);
+        $this->db->bind(':jenis_komplain', $jenis_komplain);
+        $this->db->bind(':tipe_komplain', $tipe_komplain);
+        $this->db->bind(':deskripsi_komplain', $deskripsi_komplain);
+        $this->db->bind(':tggl_komplain', $tggl_komplain);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
