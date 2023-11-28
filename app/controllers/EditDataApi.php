9781ab2a74d70ccf018d1eaad33bdb09a2a94124
Debby Az-Zahra Firdauzi <78738330+hakimlutfi46@users.noreply.github.com>
2023-11-28 22:11:45 +0700

    Add API confirm old pass & check pass

--- app/controllers/EditDataApi.php
+++ app/controllers/EditDataApi.php
@@ -37,4 +37,79 @@ class EditDataApi extends Controller
         }
         echo json_encode($response);
     }
+
+    public function checkPassword($idUser) {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Ambil password dari form data
+            $password = $_POST['password'];
+    
+            $edit_user_model = $this->model('EditDataApi_model');
+    
+            // Dapatkan password yang terenkripsi dari database berdasarkan idUser
+            $storedPassword = $edit_user_model->getStoredPassword($idUser);
+    
+            // Periksa apakah password yang dimasukkan cocok dengan password yang tersimpan
+            if (password_verify($password, $storedPassword)) {
+                $response = array(
+                    'code' => 200,
+                    'status' => 'Password benar',
+                );
+            } else {
+                $response = array(
+                    'code' => 400,
+                    'status' => 'Password salah',
+                );
+            }
+    
+            echo json_encode($response);
+        } else {
+            $response = array(
+                'code' => 404,
+                'status' => 'Metode request tidak diizinkan',
+            );
+            echo json_encode($response);
+        }
+    }
+    
+    
+
+    public function editPass($idUser){
+        if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+            $putData = file_get_contents('php://input');
+            $data = json_decode($putData, true);
+    
+            // Periksa apakah data tidak kosong dan terdapat kunci 'password' di dalamnya
+            if (isset($data['password']) && !empty($data['password'])) {
+                $password = $data['password']; // Ambil kata sandi baru dari data yang dikirim
+    
+                $edit_user_model = $this->model('EditDataApi_model');
+    
+                // Lakukan update password dengan model yang tepat
+                $success = $edit_user_model->updatePassword($password, $idUser);
+    
+                if ($success) {
+                    $response = array(
+                        'code' => 200,
+                        'status' => 'Update Kata Sandi Sukses',
+                    );
+                } else {
+                    $response = array(
+                        'code' => 400,
+                        'status' => 'Gagal mengubah kata sandi',
+                    );
+                }
+            } else {
+                $response = array(
+                    'code' => 400,
+                    'status' => 'Data password tidak ditemukan atau kosong',
+                );
+            }
+        } else {
+            $response = array(
+                'code' => 404,
+                'status' => 'Metode request tidak diizinkan',
+            );
+        }
+        echo json_encode($response);
+    }
 }
