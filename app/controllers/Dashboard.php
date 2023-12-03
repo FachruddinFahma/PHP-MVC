af8ba323018ad3637d746f3a94e7567c7fc31404
farhanariyanto03 <ariyantofarhan03@gmail.com>
2023-12-03 22:37:22 +0700

    dashboard pemilik kost

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -1,13 +1,19 @@
 <?php
-class Dashboard extends Controller
-{
-    public function index()
+    class Dashboard extends Controller
     {
-        $data['judul'] = 'Dashboard';
-        //$data['dashboard'] = $this->model('Dashboard_model')->getAllPenghuni();
-        $this->view('templates/header', $data);
-        $this->view('pemilik_kost/dashboard/index', $data);
-        $this->view('templates/footer');
+        public function index()
+        {
+            $id_user = $_SESSION['id_user'];
+        
+            $data['judul'] = 'Dashboard';
+            $dashboard_model = $this->model('Dashboard_model');
+            $data['total_komplain'] = $dashboard_model->totalKomplain();
+            $data['total_penghuni'] = $dashboard_model->totalPenghuniByIdUser($id_user);
+            $data['total_kamar'] = $dashboard_model->totalKamarForUser($id_user); // Corrected method name
+        
+            $this->view('templates/header', $data);
+            $this->view('pemilik_kost/dashboard/index', $data);
+            $this->view('templates/footer');
+        }
     }
-}
 ?>
