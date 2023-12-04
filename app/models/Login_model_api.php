ef5557555f6dcbc2e0fdc3a54ab6570c382a146c
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-12-04 10:09:20 +0700

    Update Login_model_api.php

--- app/models/Login_model_api.php
+++ app/models/Login_model_api.php
@@ -14,10 +14,14 @@ class Login_model_api
         `tb_user`.`email`, 
         `tb_user`.`password`, 
         `tb_kamar`.`id_kamar` AS 'Nomor Kamar', 
-        `tb_kamar`.`harga_bulanan`
+        `tb_kamar`.`harga_bulanan`,
+        `tb_pemesanan`.`id_pemesanan`,
+        `tb_kost`.`id_kost`     
         FROM `tb_penghuni_kamar`
         JOIN `tb_user` ON tb_penghuni_kamar.`id_user` = tb_user.`id_user`
         JOIN `tb_kamar` ON tb_penghuni_kamar.`id_kamar` = tb_kamar.`id_kamar`
+        JOIN `tb_kost` ON `tb_kost`.`id_kost` = `tb_kamar`.`id_kost`
+        JOIN `tb_pemesanan` ON `tb_pemesanan`.`id_user` = tb_user.`id_user`
         WHERE `tb_user`.`email` = :email");
         $this->db->bind(':email', $email);
         return $this->db->single();
@@ -29,10 +33,14 @@ class Login_model_api
         `tb_user`.`email`, 
         `tb_user`.`password`, 
         `tb_kamar`.`id_kamar` AS 'Nomor Kamar', 
-        `tb_kamar`.`harga_bulanan`
+        `tb_kamar`.`harga_bulanan`,
+        `tb_pemesanan`.`id_pemesanan`,
+        `tb_kost`.`id_kost`     
         FROM `tb_penghuni_kamar`
         JOIN `tb_user` ON tb_penghuni_kamar.`id_user` = tb_user.`id_user`
         JOIN `tb_kamar` ON tb_penghuni_kamar.`id_kamar` = tb_kamar.`id_kamar`
+        JOIN `tb_kost` ON `tb_kost`.`id_kost` = `tb_kamar`.`id_kost`
+        JOIN `tb_pemesanan` ON `tb_pemesanan`.`id_user` = tb_user.`id_user`
         WHERE `tb_user`.`email` = :email AND id_role = 3");
         $this->db->bind(':email', $email);
 
