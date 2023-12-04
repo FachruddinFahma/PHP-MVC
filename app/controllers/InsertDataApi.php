3df819f9d2c7ba23d0274fe202a527d6abc7714d
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-12-05 00:20:53 +0700

    ganti id_kost jadi kamar di komplain API

--- app/controllers/InsertDataApi.php
+++ app/controllers/InsertDataApi.php
@@ -8,13 +8,13 @@ class InsertDataApi extends Controller
         $insertModel = $this->model('InsertDataApi_model');
 
         $id_komplain = $_POST['id_komplain'] ?? '';
-        $id_kost = $_POST['id_kost'] ?? '';
+        $id_kamar = $_POST['id_kamar'] ?? '';
         $jenis_komplain = $_POST['jenis_komplain'] ?? '';
         $tipe_komplain = $_POST['tipe_komplain'] ?? '';
         $deskripsi_komplain = $_POST['deskripsi_komplain'] ?? '';
         $tggl_komplain = $_POST['tggl_komplain'] ?? '';
 
-        $result = $insertModel->addKomplain($id_komplain, $idUser, $id_kost, $jenis_komplain, $tipe_komplain, $deskripsi_komplain, $tggl_komplain);
+        $result = $insertModel->addKomplain($id_komplain, $idUser, $id_kamar, $jenis_komplain, $tipe_komplain, $deskripsi_komplain, $tggl_komplain);
 
         if ($result) {
             echo json_encode(array('message' => 'Data komplain berhasil ditambahkan'));
