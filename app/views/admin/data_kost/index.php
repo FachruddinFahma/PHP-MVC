ccfb877e5d36f8f30de2a3c6bae460c1d38fc26d
fachruddin <118855404+FachruddinFahma@users.noreply.github.com>
2023-12-05 04:47:46 +0700

    data kost admin hampir fix

--- app/views/admin/data_kost/index.php
+++ app/views/admin/data_kost/index.php
@@ -11,12 +11,12 @@
     <body>
         <section id="home" class="content">
             <div id="container-button" class="w-full">
-                <div id="container-button-sudahVerifikasi">
-                    <h4 id="button-sudahVerifikasi">Kost Sudah Diverifikasi</h4>
+                <div id="container-button-belumVerifikasi">
+                    <h4 id="button-belumVerifikasi">Kost belum Diverifikasi</h4>
                     <span class="line-active"></span>
                 </div>
-                <div id="container-button-belumVerifikasi">
-                    <h4 id="button-belumVerifikasi">Kost Belum Diverifikasi</h4>
+                <div id="container-button-sudahVerifikasi">
+                    <h4 id="button-sudahVerifikasi">Kost sudah Diverifikasi</h4>
                     <!-- <span class="line-active"></span> -->
                 </div>
             </div>
@@ -26,7 +26,7 @@
             </div>
             <div class="col-md-8e p-5">
                 <div class="card-body bg-white p-4" style="border-radius: 18px;">
-                    <table class="table table-hover small" class="dataDiverifikasi" id="tableVerifikasi">
+                    <table class="table table-hover small" class="dataDiverifikasi" id="tableBelumVerifikasi">
                         <thead>
                             <tr>
                                 <th scope="col">Nama Kost</th>
@@ -52,28 +52,78 @@
                             <?php endforeach; ?>
                         </tbody>
                     </table>
+
+                    <table class="table table-hover small" class="dataDiverivikasi" id="tableVerifikasi">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nama Kost</th>
+                                <th scope="col">Pemilik</th>
+                                <th scope="col">Jumlah Kamar</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($data['kostTerkonfirmasi'] as $kost) : ?>
+                                <tr>
+                                    <td><?= $kost['nama_kost']; ?></td>
+                                    <td><?= $kost['nama_lengkap']; ?></td>
+                                    <td><?= $kost['jumlah_kamar']; ?></td>
+                                    <td><?= $kost['status']; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </section>
         <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
         <script>
             $(document).ready(function() {
-                $("#tablePemilik").show();
-                $("#tablePenyewa").hide();
+                $("#tableBelumVerifikasi").show();
+                $("#tableVerifikasi").hide();
                 $("#pemesanan_content_terkonfirmasi").hide();
 
                 var initialPosition = $(".line-active").position().left;
 
-                $("#button-pemilik").click(function() {
-                    $("#tablePemilik").show();
-                    $("#tablePenyewa").hide();
+                $("#button-belumVerifikasi").click(function() {
+                    $("#tableBelumVerifikasi").show();
+                    $("#tableVerifikasi").hide();
                 });
 
-                $("#button-penyewa").click(function() {
-                    $("#tablePenyewa").show();
-                    $("#tablePemilik").hide();
+                $("#button-sudahVerifikasi").click(function() {
+                    $("#tableVerifikasi").show();
+                    $("#tableBelumVerifikasi").hide();
                 });
             });
         </script>
+
+<script>
+            new DataTable("#tableVerifikasi", {
+                info: false,
+                "columnDefs": [{
+                        "orderable": true,
+                        "targets": 0
+                    },
+                    {
+                        "orderable": false,
+                        "targets": 1
+                    },
+                    {
+                        "orderable": true,
+                        "targets": 2
+                    },
+                    {
+                        "orderable": false,
+                        "targets": 3
+                    },
+                    {
+                        "orderable": false,
+                        "targets": 4
+                    },
+                ]
+            });
+        </script>
     </body>
-</html>
+    </html>
