2329d18e1a769060928bcb9232ccf53d791fedb7
farhanariyanto03 <ariyantofarhan03@gmail.com>
2023-12-05 09:40:22 +0700

    pemilik kost sudah selesai semua

--- app/models/Pemesanan_Model.php
+++ app/models/Pemesanan_Model.php
@@ -10,7 +10,12 @@ class Pemesanan_Model
 
     public function getPemesananMasuk()
     {
-        $this->db->query("SELECT * FROM tb_pemesanan WHERE status = 'masuk'");
+        $this->db->query("SELECT tb_pemesanan.id_pemesanan, tb_kamar.id_kamar, tb_kamar.nama_kamar, tb_user.id_user, tb_user.nama_lengkap, tb_pemesanan.tggl_pemesaan, 
+        tb_pemesanan.tggl_masuk, tb_pemesanan.tggl_keluar, tb_pemesanan.harga, tb_pemesanan.kategori, tb_pemesanan.metode_pembayaran, tb_pemesanan.bukti_tf, tb_pemesanan.status
+        FROM tb_pemesanan
+        JOIN tb_user ON tb_user.id_user = tb_pemesanan.id_user
+        JOIN tb_kamar ON tb_kamar.id_kamar = tb_pemesanan.id_kamar
+        WHERE tb_pemesanan.status = 'masuk'");
         return $this->db->resultSet();
     }
 
@@ -22,8 +27,27 @@ class Pemesanan_Model
 
     public function terimaPesanan($id_pemesanan)
     {
-        $this->db->query("UPDATE `tb_pemesanan` SET `status` = 'terkonfirmasi' WHERE `id_pemesanan` = '$id_pemesanan'");
-        $this->db->execute();
+        // Ambil data `id_user` dan `id_kamar` dari tabel `tb_pemesanan`
+        $this->db->query("SELECT id_user, id_kamar FROM tb_pemesanan WHERE id_pemesanan = :id_pemesanan");
+        $this->db->bind(':id_pemesanan', $id_pemesanan);
+        $result = $this->db->single();
+
+        // Pastikan data ditemukan
+        if ($result) {
+            $id_user = $result['id_user'];
+            $id_kamar = $result['id_kamar'];
+
+            // Update status pemesanan
+            $this->db->query("UPDATE `tb_pemesanan` SET `status` = 'terkonfirmasi' WHERE `id_pemesanan` = :id_pemesanan");
+            $this->db->bind(':id_pemesanan', $id_pemesanan);
+            $this->db->execute();
+
+            // Tambahkan data ke dalam `tb_penghuni_kamar`
+            $this->db->query("INSERT INTO `tb_penghuni_kamar` (`id_user`, `id_kamar`) VALUES (:id_user, :id_kamar)");
+            $this->db->bind(':id_user', $id_user);
+            $this->db->bind(':id_kamar', $id_kamar);
+            $this->db->execute();
+        }
     }
 
     public function tolakPesanan($id_pemesanan)
