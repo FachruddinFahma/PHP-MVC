6a8f93c4c0f76acb0a5f65c63693ff5a801c35c5
fachruddin <118855404+FachruddinFahma@users.noreply.github.com>
2023-12-05 09:43:27 +0700

    Testimoni Admin belum fix

--- app/controllers/Testimoni_Admin.php
+++ app/controllers/Testimoni_Admin.php
@@ -4,6 +4,7 @@ class Testimoni_Admin extends Controller
     public function index()
     {
         $data['judul'] = 'Testimoni';
+        $data['Testimoni'] = $this->model('testimoniAdmin_model')->getDataTestimoni();
         $this->view('templatesAdmin/header', $data);
         $this->view('admin/testimoni/index', $data);
         $this->view('templatesAdmin/footer');
--- app/views/admin/testimoni/index.php
+++ app/views/admin/testimoni/index.php
@@ -4,12 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman <?php echo $data['judul']; ?></title>
+    <title>Document</title>
 </head>
 
 <body>
     <section id="home" class="content">
-        <h1>Halaman Testimoni</h1>
+        <div class="col-md-8e p-5">
+            <div class="card-body bg-white p-4" style="border-radius: 18px;">
+                <table class="table table-hover small" id="dataTestimoni">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id User</th>
+                            <th style="width:650px" scope="col">Deskripsi Testimoni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data['Testimoni'] as $d) : ?>
+                            <tr>
+                                <td><?= $d['id_user'] ?></td>
+                                <td><?= $d['deskripsi_testi'] ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </section>
 </body>
 
